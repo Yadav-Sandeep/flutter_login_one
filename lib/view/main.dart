15f50c25eea12d798804e0460657b012eeb250d4
KrutarthK <KrutarthK@nsdlbank.co.in>
2022-06-06 16:47:01 +0530

    bytearray code pushed

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -132,6 +132,17 @@ class _MyAppState extends State<MyApp> implements LoginContract{
                             String checksumSeq = channelId + appId + partnerId + mobileNo + type +
                                 email + password + token;
 
+                            //convert into byteArray using below three methods
+                            /*List<int> bytes = utf8.encode(passKey);
+                          print(bytes11);*/
+
+                            /*List<int> bytes = passKey.codeUnits;
+                          print(bytes);*/
+
+                            List<int> bytes = passKey.runes.toList();
+                            print(bytes);
+
+                            //Utf8Encoder:-This class converts strings to their UTF-8 code units (a list of unsigned 8-bit integers).
                             List<int> messageBytes = Utf8Encoder().convert(checksumSeq);
                             List<int> key = Utf8Encoder().convert(passKey);
                             Hmac hmac = Hmac(sha512, key);
