abcf3b1cc9295e649203961ca68fca394805e4c7
Sandeepy <sandeepy@nsdlbank.co.in>
2022-06-14 13:04:21 +0530

    native code done

--- android/app/src/main/kotlin/com/example/flutter_login_one/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_login_one/MainActivity.kt
@@ -1,6 +1,31 @@
 package com.example.flutter_login_one
 
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "nativeCall";
+
+    var textFromFlutter: String = ""
+
+    private lateinit var channel: MethodChannel
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        //textFromFlutter = call.argument("text");
+
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,CHANNEL)
+        
+        channel.setMethodCallHandler { call, result ->
+
+            if(call.method == "showMyToast"){
+                textFromFlutter = call.argument<String>("text").toString()
+                Toast.makeText(this,textFromFlutter + "..From Native..",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
